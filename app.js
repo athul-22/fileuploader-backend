72c9addfaed4ea235200bedc988d45f1167dbda0
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-12-08 11:12:38 +0530

    v1

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const cloudinary = require('cloudinary').v2;
 const app = express();
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }));
 app.use(express.json());
 
 // Connect to MongoDB
@@ -38,36 +38,29 @@ const storage = multer.diskStorage({
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/') || file.mimetype === 'application/pdf') {
-    cb(null, true);
-  } else {
-    cb(new Error('Invalid file type. Only images and PDFs are allowed.'));
-  }
-};
-
-const upload = multer({ storage, fileFilter });
+const upload = multer({ storage });
 
 const File = mongoose.model('File', {
   originalname: String,
   filename: String,
   mimetype: String,
-  fileLink: String,
+  fileLink: String, // Keep the field for now
 });
 
 app.post('/upload', upload.single('file'), async (req, res) => {
   try {
     // Upload the file to Cloudinary
     const cloudinaryUpload = await cloudinary.uploader.upload(req.file.path, {
-      folder: 'your_folder_name',
-      public_id: `${Date.now()}_${req.file.originalname}`,
+      folder: 'your_folder_name', // Specify the folder in Cloudinary account
+      public_id: `${Date.now()}_${req.file.originalname}`, // Use a unique public_id
+      quality:50
     });
 
     const file = new File({
       originalname: req.file.originalname,
       filename: cloudinaryUpload.public_id,
       mimetype: req.file.mimetype,
-      fileLink: cloudinaryUpload.secure_url,
+      fileLink: cloudinaryUpload.secure_url, // Update fileLink to Cloudinary URL
     });
 
     await file.save();
@@ -81,17 +74,20 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 });
 
 app.get('/files', async (req, res) => {
-  try {
-    const files = await File.find();
-    res.status(200).json(files);
-  } catch (error) {
-    console.error('Error fetching files:', error);
-    res.status(500).json({ error: 'Error fetching files', details: error.message });
-  }
-});
+    try {
+      const files = await File.find();
+      res.status(200).json(files);
+    } catch (error) {
+      console.error('Error fetching files:', error);
+      res.status(500).json({ error: 'Error fetching files', details: error.message });
+    }
+  });
 
+// Serve static files
 app.use('/public', express.static('./public'));
 
 app.listen(3001, () => {
   console.log('Server is running');
 });
+
+
